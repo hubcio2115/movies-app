f9f040f692b0e078cb94125e1732d9a78e8e06c4
hubcio2115 <kowalski.hubercik1@gmail.com>
2021-12-20 16:05:12 +0100

    initial commit

---
+++ wyszukiwarka-filmow-frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const App: FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">Movies App</header>
+    </div>
+  );
+};
+
+export default App;
