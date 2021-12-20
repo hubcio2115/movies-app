ea36e2cf55c45503a2ff0e237a7bc5c3fa7aa354
hubcio2115 <kowalski.hubercik1@gmail.com>
2021-12-20 16:53:21 +0100

    added NoMovies

--- wyszukiwarka-filmow-frontend/src/components/movies-view/MoviesView.tsx
+++ wyszukiwarka-filmow-frontend/src/components/movies-view/MoviesView.tsx
@@ -3,12 +3,16 @@ import { FC } from "react";
 import MoviesOptions from "./MoviesOptions";
 import MoviesList from "./MoviesList";
 import MoviesFavorites from "./MoviesFavorites";
+import NoMovies from "./NoMovies";
 
 const MoviesView: FC = () => {
+  const movies = false;
+
   return (
     <div>
+      <h1>Lista Filmów</h1>
       <MoviesOptions />
-      <MoviesList />
+      {movies ? <MoviesList /> : <NoMovies />}
       <MoviesFavorites />
     </div>
   );
