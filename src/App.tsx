06d3f4a2f6004826bffc1cbea28a3728a7b463ed
hubcio2115 <kowalski.hubercik1@gmail.com>
2021-12-30 17:04:17 +0100

    moved movie rendering to MovieCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
-import { FC, useEffect, useState } from "react";
+import { FC } from "react";
 import { Routes, Route } from "react-router-dom";
-import api from "./api/movies";
 
 import MyHeader from "./components/MyHeader";
 import MoviesView from "./components/movies-view/MoviesView";
 import MovieAddForm from "./components/movie-add-form/MovieAddForm";
 
 const App: FC = () => {
-  const [movies, setMovies] = useState([{}]);
-
-  const getMovies = async () => {
-    return await api.get("/movies");
-  };
-
-  useEffect(() => {
-    const getAllMovies = async () => {
-      const allMovies = await getMovies();
-      if (allMovies) setMovies(allMovies.data);
-    };
-
-    getAllMovies();
-  }, []);
-
   return (
     <div className="App">
       <MyHeader />
