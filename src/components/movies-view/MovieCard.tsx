84d0154a1427105a4629b6921b563757393e59dc
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-01-01 16:06:00 +0100

    transfered movie interface into seperate file

--- src/components/movies-view/MovieCard.tsx
+++ src/components/movies-view/MovieCard.tsx
@@ -1,16 +1,8 @@
 import { FC } from "react";
+import Movie from "../../interfaces/Movie";
+
 interface Props {
-  movie: {
-    id: number;
-    title: string;
-    director: string;
-    genre: string;
-    year: number;
-    description: string;
-    image_url: string;
-    rating_count: number;
-    rating: number;
-  };
+  movie: Movie;
 }
 
 const MovieCard: FC<Props> = ({ movie }) => {
--- src/components/movies-view/MoviesOptions.tsx
+++ src/components/movies-view/MoviesOptions.tsx
@@ -1,22 +1,69 @@
-import { FC, Dispatch, SetStateAction } from "react";
+import {
+  FC,
+  useState,
+  Dispatch,
+  SetStateAction,
+  ChangeEventHandler,
+  ChangeEvent,
+} from "react";
 import { Link } from "react-router-dom";
+import Movie from "../../interfaces/Movie";
 
 interface Props {
+  sortedMovies: Movie[];
+  setSortedMovies: Dispatch<SetStateAction<Movie[]>>;
   filterTitle: string;
   setFilterTitle: Dispatch<SetStateAction<string>>;
 }
 
-const MoviesOptions: FC<Props> = ({ filterTitle, setFilterTitle }) => {
+const MoviesOptions: FC<Props> = ({
+  sortedMovies,
+  setSortedMovies,
+  filterTitle,
+  setFilterTitle,
+}) => {
+  const [selectedValue, setSelectedValue] = useState<
+    "title" | "genre" | "year" | ""
+  >("");
+
+  const handleSort: ChangeEventHandler = (
+    e: ChangeEvent<HTMLSelectElement>
+  ) => {
+    setSelectedValue(e.target.value as "title" | "genre" | "year");
+    let temp = sortedMovies;
+
+    temp.sort((current, next) => {
+      if (
+        current[selectedValue as "title" | "genre" | "year"] >
+        next[selectedValue as "title" | "genre" | "year"]
+      ) {
+        return 1;
+      } else if (
+        current[selectedValue as "title" | "genre" | "year"] ===
+        next[selectedValue as "title" | "genre" | "year"]
+      ) {
+        return 0;
+      }
+      return -1;
+    });
+
+    setSortedMovies(temp);
+  };
+
   return (
     <div>
       <Link to={"/add-form"}>
         <button>Dodaj Film</button>
       </Link>
-      <select name="category" id="category" placeholder="">
+      <select
+        name="category"
+        id="category"
+        value={selectedValue}
+        onChange={(e) => handleSort(e)}
+      >
         <option value="" disabled>
           Sortuj wg.
         </option>
-        <option value="date">Data dodania</option>
         <option value="title">Tytuł</option>
         <option value="genre">Gatunek</option>
         <option value="year">Rok</option>
