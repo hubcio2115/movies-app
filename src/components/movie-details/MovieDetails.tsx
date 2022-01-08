5d8b426db9451203a878f6f3b499a54b57decfa6
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-01-08 15:01:26 +0100

    added a from for editing a movie

--- src/components/movie-details/MovieDetails.tsx
+++ src/components/movie-details/MovieDetails.tsx
@@ -1,12 +1,15 @@
 import { FC, useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import api from "../../api/movies";
+import * as Yup from "yup";
 
 import Movie from "../../interfaces/Movie";
+import { Field, Form, Formik } from "formik";
 
 const MovieDetails: FC = () => {
   const params = useParams();
   const [movie, setMovies] = useState<Movie>({} as Movie);
+  const [isEditing, setIsEditing] = useState(false);
 
   const getMovies = async () => {
     return await api.get(`/movie/${params.movieId}`);
@@ -21,20 +24,105 @@ const MovieDetails: FC = () => {
     getAllMovies();
   });
 
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required("Pole wymagane"),
+    director: Yup.string().required("Pole wymagane"),
+    genre: Yup.string().required("Pole wymagane"),
+    year: Yup.number()
+      .min(1000, "Proszę podać date po roku 1000")
+      .max(
+        new Date().getFullYear(),
+        "Rok nie może być większy niż z dzisiejszej daty"
+      )
+      .required("Pole wymagane"),
+    description: Yup.string().required("Pole wymagane"),
+    image_url: Yup.string().url("Proszę podać ").nullable(),
+  });
+
   return (
     <div>
       <h2>Szczegóły filmu</h2>
       <Link to={"/"}>
         <button>Powrót</button>
       </Link>
-      <div style={{ width: "300px" }}>
-        <img src={movie.image_url} alt="movie-poster" />
-        <p>{movie.title}</p>
-        <p>{movie.director}</p>
-        <p>{movie.genre}</p>
-        <p>{movie.year}</p>
-        <p>{movie.description}</p>
-      </div>
+      <button
+        onClick={() => {
+          setIsEditing(!isEditing);
+        }}
+      >
+        Edytuj
+      </button>
+      {isEditing ? (
+        <Formik
+          initialValues={movie}
+          validationSchema={validationSchema}
+          onSubmit={(values) => {
+            api.put(`/movie/${movie.id}`, values);
+            setIsEditing(!isEditing);
+          }}
+        >
+          {({ errors, touched }) => (
+            <Form>
+              <div>
+                <label htmlFor="title">Tytuł: </label>
+                <Field id="title" name="title" type="text" />
+                {errors.title && touched.title ? (
+                  <div>{errors.title}</div>
+                ) : null}
+              </div>
+              <div>
+                <label htmlFor="director">Dyrektor: </label>
+                <Field id="director" name="director" type="text" />
+                {errors.director && touched.director ? (
+                  <div>{errors.director}</div>
+                ) : null}
+              </div>
+              <div>
+                <label htmlFor="genre">Gatunek: </label>
+                <Field id="genre" name="genre" type="text" />
+                {errors.genre && touched.genre ? (
+                  <div>{errors.genre}</div>
+                ) : null}
+              </div>
+              <div>
+                <label htmlFor="year">Rok premiery: </label>
+                <Field id="year" name="year" type="number" />
+                {errors.year && touched.year ? <div>{errors.year}</div> : null}
+              </div>
+              <div>
+                <label htmlFor="description">Opis: </label>
+                <Field
+                  as="textarea"
+                  id="description"
+                  name="description"
+                  rows={10}
+                  cols={30}
+                />
+                {errors.description && touched.description ? (
+                  <div>{errors.description}</div>
+                ) : null}
+              </div>
+              <div>
+                <label htmlFor="image_url">Zdjęcie: </label>
+                <Field id="image_url" name="image_url" type="url" />
+                {errors.image_url && touched.image_url ? (
+                  <div>{errors.image_url}</div>
+                ) : null}
+              </div>
+              <button type="submit">Zapisz</button>
+            </Form>
+          )}
+        </Formik>
+      ) : (
+        <div style={{ width: "300px" }}>
+          <img src={movie.image_url} alt="movie-poster" />
+          <p>{movie.title}</p>
+          <p>{movie.director}</p>
+          <p>{movie.genre}</p>
+          <p>{movie.year}</p>
+          <p>{movie.description}</p>
+        </div>
+      )}
     </div>
   );
 };
