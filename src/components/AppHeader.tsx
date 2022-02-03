9ab00ea3c9b0152314e15fead61cb7058abf44a6
hubcio2115 <kowalski.hubercik1@gmail.com>
2022-02-03 23:24:17 +0100

    changed app header

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,10 +1,20 @@
 import { FC } from "react";
 
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
 const AppHeader: FC = () => {
   return (
-    <header>
-      <h1>Movies App</h1>
-    </header>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <img src="favicon.png" alt="logo" width={25} height={25} />
+          <Typography variant="h6" ml={1} height={28}>
+            MoviesApp
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
